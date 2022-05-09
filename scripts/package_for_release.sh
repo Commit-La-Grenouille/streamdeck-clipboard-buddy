e1e90d443089a0c315be891f0f2891e65e1c3a25
Commit-La-Grenouille <dr_no7@caramail.com>
2022-05-09 18:04:56 +0200

    SCRIPT: updating releasing script to detect old releases, rename & tag

--- scripts/package_for_release.sh
+++ scripts/package_for_release.sh
@@ -5,9 +5,19 @@ set -e
 # Defining where we are
 HERE=$(dirname "$0")
 
+# Define the version we release
+VERS=$1
+
+if [ "${VERS}" = "" ];
+then
+    echo "You must provide a version as first argument !"
+    exit 1
+fi
+
 # And some more data useful for this script
 DTZ="DistributionToolMac.zip"
-PLUGIN="net.localhost.streamdeck.clipboard-buddy.sdPlugin"
+PLUGIN="net.localhost.streamdeck.clipboard-buddy"
+EXT="streamDeckPlugin"
 
 
 # Grabbingt the distribution tool if not yet present
@@ -20,5 +30,15 @@ if [[ ! -e "${HERE}/DistributionTool" ]]; then
     rm "${HERE}/${DTZ}"
 fi
 
+if [ -e  "${HERE}/../${PLUGIN}.${EXT}" ];
+then
+    echo "Unable to release ${PLUGIN}.${EXT} as such release already exists on the disk !"
+    exit 1
+fi
+
 # Running the distribution tool to transform our open-plugin folder into a proper plugin archive
-"${HERE}/DistributionTool" -b -i "Sources/${PLUGIN}" -o "${HERE}/.."
+"${HERE}/DistributionTool" -b -i "Sources/${PLUGIN}.sdPlugin" -o "${HERE}/.."
+
+# Then moving the release out of the way by renaming it with the version we will tag
+git tag "${VERS}"
+mv "${HERE}/../${PLUGIN}.${EXT}" "${HERE}/../${PLUGIN}.v${VERS}.${EXT}"
